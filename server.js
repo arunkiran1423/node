be5b67ea00c2dde39c81fd67e12a061e8297425d
arun <arunkiran1423@gmail.com>
2018-09-03 03:25:17 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const hbs = require("hbs");
+
+const app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  console.log(`${now}:`);
+  next();
+});
+
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+  res.render("home.hbs", {
+    title: "Home pagee",
+    date: new Date().getFullYear()
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.hbs");
+});
+
+app.listen(3000);
